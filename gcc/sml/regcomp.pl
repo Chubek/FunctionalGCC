8179827549c9954f08dbf5b584ab775d92b20ce1
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-13 02:17:59 +0330

    added to regex compiler: the infix to postfix converter as per Thompson paper

--- gcc/sml/regcomp.pl
+++ gcc/sml/regcomp.pl
@@ -1,11 +1,5 @@
-#!/usr/bin/env perl
 
-# RegComp is a small preprocessor similar to re2c --- it takes a preprocessor directive containing a regular expressison, converts that regular expression to NFA, the converts that NFA to DFA, and then compiles it to C code.
-# You must RegComp a file and it will bypass any line that is not prefixed with '==='. Th lines prefixed by '===' get preprocessed into compiled RE. It requires several patameters:
-# === regex::"<regex>" buff::"<buffername>" next::"<get next char in buffer"
 
-use strict;
-use warnings;
 use Data::Dumper;
 
 my @directives;
@@ -25,30 +19,30 @@ sub infix_to_postfix {
     my ($infix_expression) = @_;
 
     my %precedence = (
-        '|' => 1,        # Alternation
-        '.' => 2,        # Concatenation
-        '*' => 3,        # Zero or more repetitions
-        '+' => 3,        # One or more repetitions
-        '?' => 3,        # Optional 
-	'-' => 2, 	 # Character class
+        '|' => 1,
+        '.' => 2,
+        '*' => 3,
+        '+' => 3,
+        '?' => 3,
+    );
+
+    my %associativity = (
+        '|' => 'left',
+        '.' => 'left',
+        '*' => 'right',
+        '+' => 'right',
+        '?' => 'right',
     );
 
     my @output;
     my @stack;
 
-    # Split the infix expression into tokens
     my @tokens = split /\s*/, $infix_expression;
 
     for my $token (@tokens) {
-        if ($token =~ /\w|\-/) {
+        if ($token =~ /[^\(\)\*\+\?\|\.]/) {
             push @output, $token;
-        } elsif ($token eq '[') {
-            push @stack, $token;
-        } elsif ($token eq ']') {
-	    while (@stack && $stack[-1] ne ']') {
-		push @output, pop @stack;
-	    }	
-	} elsif ($token eq '(') {
+        } elsif ($token eq '(') {
             push @stack, $token;
         } elsif ($token eq ')') {
             while (@stack && $stack[-1] ne '(') {
@@ -56,7 +50,14 @@ sub infix_to_postfix {
             }
             pop @stack;
         } elsif ($precedence{$token}) {
-            while (@stack && $precedence{$stack[-1]} >= $precedence{$token}) {
+            while (
+                @stack
+                && (
+                    $precedence{$stack[-1]} > $precedence{$token}
+                    || ($precedence{$stack[-1]} == $precedence{$token}
+                        && $associativity{$token} eq 'left')
+                )
+            ) {
                 push @output, pop @stack;
             }
             push @stack, $token;
@@ -64,11 +65,8 @@ sub infix_to_postfix {
     }
 
     while (@stack) {
-        push @output, pop @stack;  # Pop remaining operators
+        push @output, pop @stack;
     }
 
     return join(' ', @output);
 }
-
-
-print infix_to_postfix("ab[a-z]b*k+");
